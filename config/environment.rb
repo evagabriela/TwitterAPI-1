e7cdac04aec96f8965503083b8fd0867309b649d
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-09 16:14:57 -0700

    mod config

--- config/environment.rb
+++ config/environment.rb
@@ -40,7 +40,7 @@ env_config.each do |key, value|
   ENV[key] = value
 end
 
-@user = Twitter.configure do |config|
+Twitter.configure do |config|
   config.consumer_key = ENV['CONSUMER_KEY']
   config.consumer_secret = ENV['CONSUMER_SECRET']
   config.oauth_token = ENV['USER_ACCESS_TOKEN']
