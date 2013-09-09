7d0121208c89b68cd85ced4f5448e2c1c1864a9b
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-09 16:28:50 -0700

    create models and migration

--- db/migrate/20130909161549_create_tweets.rb
+++ db/migrate/20130909161549_create_tweets.rb
@@ -1,4 +1,11 @@
 class CreateTweets < ActiveRecord::Migration
+  
   def change
+     create_table :tweets do |t|
+      t.text :message
+      t.time :tweet_time
+      t.belongs_to :user
+      t.timestamps
+    end
   end
 end
