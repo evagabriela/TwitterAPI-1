2c1adf0a8f4a4db3f89670498940243a1b3bcd23
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-09 18:26:43 -0700

    update user at over_time

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,6 +9,9 @@ end
 get '/:twitter_handler' do
   @user = User.find_by_name(params[:twitter_handler])
   @user = log_user(params[:twitter_handler]) if @user.nil?
+
+  @user = update_user(@user) if over_time?(@user, 1)
+
   @tweets = @user.find_tweets 
   erb :tweets
 end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,9 +4,19 @@ def log_user(username)
   new_user
 end
 
-def fetch_new_tweets(username)
-  tweets = Twitter.user_timeline(username.name).map { |t| t.text }
+def fetch_new_tweets(user)
+  tweets = Twitter.user_timeline(user.name).map { |t| t.text }
   tweets.each do |tweet|
-    username.tweets << Tweet.create(message: tweet)
+    user.tweets << Tweet.create(message: tweet)
   end
+end
+
+def over_time?(user, time_interval)
+  Time.now - user.created_at >= (time_interval * 60)
+end
+
+def update_user(user)
+  username = user.name
+  user.destroy
+  log_user(username)
 end
